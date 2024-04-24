72a6858ebfe993bcec72048a6202df36d470d45d
lavanand <142693591+lavanand@users.noreply.github.com>
2024-04-24 00:44:31 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c;
+            if(radioButton1.Checked)
+            {
+                c = a + b;
+                label4.Text = "Total="+c.ToString();
+            }
+            if (radioButton2.Checked)
+            {
+                c = a - b;
+                label4.Text = "sub= " + c.ToString();
+            }
+            if (radioButton3.Checked)
+            {
+                c = a * b;
+                label4.Text = "Multiple= " + c.ToString();
+            }
+            if (radioButton4.Checked)
+            {
+                c = a / b;
+                label4.Text = "Divide= " + c.ToString();
+            }
+            if (radioButton5.Checked)
+            {
+                c = a % b;
+                label4.Text = "Mod= " + c.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label4.Text = "";
+        }
+    }
+}
